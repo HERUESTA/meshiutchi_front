d38ef984cd76c53a1d3400e8a1cd42f580293af7
jyasco <szkmrk715@outlook.jp>
2025-05-04 13:15:53 +0900

    トップページの大枠作成

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
-  <div>Hello from Top Page!</div>
+  <div>
+    <MoodSelector />
+  </div>
 </template>
 
-<script setup lang='ts'>
+<script setup lang="ts">
+import MoodSelector from '../components/MoodSelector.vue'
+
 </script>
